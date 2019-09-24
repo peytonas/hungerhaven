928b9b7aa85880813867c33ac7842b0487343faa
Braden Dykstra <Toastie285@gmail.com>
2019-09-24 10:44:37 -0600

    logo in the home vue

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home row">
+    <img
+      class="col-12"
+      alt="Hunger Haven Logo"
+      src="https://files.slack.com/files-pri/T1GGQN1RU-FNB7BRSRH/screen_shot_2019-09-23_at_8.39.29_pm.png"
+    />
+    <HelloWorld msg="Welcome to Your Worst Nightmare" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
