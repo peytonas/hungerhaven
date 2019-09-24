b7d7f78c02100c9ff0e8e362268de0fdae59a56e
Braden Dykstra <Toastie285@gmail.com>
2019-09-24 16:25:28 -0600

    added editprofile

---
+++ client/src/views/EditProfile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="editProfile"></div>
+</template>
+
+
+<script>
+export default {
+  name: "editProfile",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
