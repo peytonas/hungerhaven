e4319143512b803fb610f8173e78bc82b22f0b2c
SeanFuller294 <s.fuller294@hotmail.com>
2019-09-24 15:39:12 -0600

    basic main page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,30 @@
 <template>
-  <div class="home row">
-    <img class="col-12" alt="Hunger Haven Logo" src="../assets/HungerHavenLogo.png" />
-    <h1>Welcome {{user.name.split('').splice(0,1).join('').toUpperCase()}}{{user.name.split('').splice(1).join().replace(',','')}}</h1>
-    <button class="btn btn-danger" @click="logout">Logout</button>
+  <div>
+    <div class="home row justify-content-center">
+      <img class="col-12" alt="Hunger Haven Logo" src="../assets/HungerHavenLogo.png" />
+      <h1>He's dead {{user.name}}</h1>
+    </div>
+    <div class="row justify-content-center">
+      <form>
+        <label for="pin">Enter A PIN</label>
+        <div class="input-group">
+          <input type="string" class="form-control" id="PIN" placeholder="12345" />
+          <div class="input-group-append">
+            <p type="submit" class="btn btn-light">🍕</p>
+          </div>
+        </div>
+      </form>
+      <button class="btn">Host An Event</button>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-12">
+        <h5>My Events</h5>
+      </div>
+      <hr />
+    </div>
+    <div class="fixed-bottom">
+      <button class="btn btn-danger" @click="logout">Logout</button>
+    </div>
   </div>
 </template>
 
@@ -27,3 +49,4 @@ export default {
   }
 };
 </script>
+
